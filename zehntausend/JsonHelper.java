4cdb304ceb6085ba76eb06d9779112a210bc9eba
spaghettic0der <max.wittig95@gmail.com>
2016-07-19 15:47:33 +0200

    settings.json is now created by default, if not found

--- zehntausend/JsonHelper.java
+++ zehntausend/JsonHelper.java
@@ -38,6 +38,8 @@ public class JsonHelper
         catch (Exception e)
         {
             System.out.println(e);
+            Settings settings = new Settings();
+            saveSettings(settings);
         }
 
         return null;
